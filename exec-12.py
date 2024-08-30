7995235a8def61dbf7c6273a6c78e5f2bdac462c
Marcello Luz <marcelloeluz15@gmail.com>
2024-08-30 20:00:16 -0300

    Conversão de Temperatura

---
+++ exec-12.py
@@ -0,0 +1,9 @@
+F = int(input("Diga o valor da temperatura atual em Fahrenheit: "))
+print("Fazendo a conversão de Fahrenheit para °C")
+print()
+input("calculando...")
+input("calculando...")
+print()
+conversaof = (F - 32)/9
+C = 5 * conversaof
+print(f"O valor da temperatura em °C é de {C:.0f} graus celsius")
