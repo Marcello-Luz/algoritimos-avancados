2af9dc056dc04ca1b9e2517651e9f164bd1e30c5
Marcello Luz <marcelloeluz15@gmail.com>
2024-08-30 19:57:50 -0300

    Custo de um Carro

--- exec-10.py
+++ exec-10.py
@@ -1,9 +1,5 @@
-carros_vendidos = int(input("Diga o número de carros que você vendeu: "))
-valor_vendas = 700 * carros_vendidos
-print(f"O valor total de suas vendas foi de R${valor_vendas}")
-print()
-porcentagem = (5/100) * valor_vendas
-salario_fixo = int(input("Diga o valor do seu salário fixo em R$: "))
-print()
-salario_final = salario_fixo + valor_vendas + (5/100 * valor_vendas)
-print(f"O seu salário final será de {salario_final:.0f} reais")
+valor_carro = float(input("Diga o valor do carro que voçê deseja em R$: "))
+print(f"O valor do carro é R${valor_carro:.0f}?")
+resposta = input()
+custo = (28/100) * valor_carro + (45/100) * valor_carro + valor_carro
+print(f"O custo desse carro é de R${custo:.0f}")
